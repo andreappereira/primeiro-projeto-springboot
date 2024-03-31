70dbf993c8cc381a96dd9d421764c25f800539cb
Andre <andreappereira@hotmail.com>
2024-03-31 00:15:22 -0300

    first commit

---
+++ src/test/java/com/andre/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.primeiroprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
